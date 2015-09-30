6cb9040c450daa9f51feee8647351d8caeaf306b
monocasa <tristan.hastings.miller@gmail.com>
2015-09-29 20:19:06 -0600

    Objdump initial implementation.  Works for chip8/binary combination

--- src/objdump.rs
+++ src/objdump.rs
@@ -1,6 +1,18 @@
+#![feature(convert)]
+
+extern crate exefmt;
 extern crate getopts;
+extern crate opcode;
 
 use getopts::Options;
+
+use std::io::{Error, ErrorKind};
+
+use exefmt::Loader;
+
+use opcode::chip8::Chip8Disasm;
+
+use opcode::Arch;
 
 #[derive(PartialEq)]
 enum BinaryFormat {
@@ -14,9 +26,26 @@ impl Default for BinaryFormat {
 	fn default() -> BinaryFormat { BinaryFormat::NotSet }
 }
 
+#[derive(PartialEq)]
+enum Disassemble {
+	None,
+	Executable,
+	All,
+}
+
+impl Default for Disassemble {
+	fn default() -> Disassemble { Disassemble::None }
+}
+
 #[derive(Default)]
 struct ObjdumpOptions {
 	bin_fmt: BinaryFormat,
+	dis: Disassemble,
+	arch: Option<opcode::Arch>,
+
+	vma_offset: u64,
+
+	at_least_one_opt: bool,
 }
 
 enum ParseError {
@@ -38,6 +67,46 @@ impl From<getopts::Fail> for ParseError {
 type ParseResult = Result<(ObjdumpOptions, Vec<String>), ParseError>;
 
 //const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn parse_hex(string: &String) -> Option<u64> {
+	let mut base = 0;
+	let mut value: u64 = 0;
+	let v: Vec<char> = string.chars().collect();
+
+	if (v.len() > 2) && (v[0] == '0') && (v[1] == 'x') {
+		base = 2;
+	}
+
+	for character in v {
+		if base > 0 {
+			base -= 1;
+			continue;
+		}
+
+		match character {
+			'0' ... '9' => {
+				value <<= 4;
+				value += (character as u64) - ('0' as u64);
+			},
+
+			'a' ... 'f' => {
+				value <<= 4;
+				value += (character as u64) - ('a' as u64) + 0xa;
+			},
+
+			'A' ... 'F' => {
+				value <<=4;
+				value += (character as u64) - ('A' as u64) + 0xA;
+			},
+
+			_ => {
+				return None;
+			},
+		}
+	}
+
+	Some(value)
+}
 
 fn print_help()
 {
@@ -85,11 +154,14 @@ fn parse_target(targets: &Vec<String>, parsed_opts: &mut ObjdumpOptions) -> Resu
 fn parse_opts(args: &Vec<String>, opts: &mut Options) -> ParseResult {
 	let mut parsed_opts: ObjdumpOptions = Default::default();
 
+	opts.optflag("H", "help",            "Display this information");
+	opts.optflag("v", "version",         "Display the version number of readelf");
+	opts.optflag("d", "disassemble",     "Disassemble executable sections");
+	opts.optflag("D", "disassemble-all", "Disassemble all sections");
+
 	opts.optflagopt("b", "target",     "Binary format.  Use \'?\' to list",           "FORMAT");
 	opts.optflagopt("",  "adjust-vma", "Add OFFSET to all display section addresses", "OFFSET");
-
-	opts.optflag("H", "help",     "Display this information");
-	opts.optflag("v", "version",  "Display the version number of readelf");
+	opts.optflagopt("m", "machine",    "Machine architecture type",                   "MACHINE");
 
 	let matches = try!(opts.parse(&args[1..]));
 
@@ -101,8 +173,31 @@ fn parse_opts(args: &Vec<String>, opts: &mut Options) -> ParseResult {
 		return Err(ParseError::Ver);
 	}
 
+	if matches.opt_present("disassemble") {
+		parsed_opts.dis = Disassemble::Executable;
+
+		parsed_opts.at_least_one_opt = true;
+	}
+
+	if matches.opt_present("disassemble-all") {
+		parsed_opts.dis = Disassemble::All;
+
+		parsed_opts.at_least_one_opt = true;
+	}
+
+	if matches.opt_present("machine") {
+		parsed_opts.arch = match matches.opt_str("machine").unwrap().as_ref() {
+			"chip8" => Some(opcode::Arch::Chip8),
+			_ => return Err(ParseError::UnknownArgument("machine", matches.opt_str("machine").unwrap())),
+		};
+	}
+
 	if matches.opt_present("target") {
 		try!(parse_target(&matches.opt_strs("target"), &mut parsed_opts));
+	}
+
+	if matches.opt_present("adjust-vma") {
+		parsed_opts.vma_offset = parse_hex(&matches.opt_str("adjust-vma").unwrap()).unwrap();
 	}
 
 	let files = if !matches.free.is_empty() {
@@ -158,6 +253,102 @@ fn on_err(err: ParseError, program_name: &str) {
 	std::process::exit(ret_code);
 }
 
+fn segment_filter_factory(parsed_options: &ObjdumpOptions) -> Box<Fn(&exefmt::Segment) -> bool> {
+	match parsed_options.dis {
+		Disassemble::None       => Box::new(|_| false),
+		Disassemble::Executable => Box::new(|segment| segment.executable),
+		Disassemble::All        => Box::new(|_| true),
+	}
+}
+
+fn disassembler_factory(arch_type: &Option<opcode::Arch>) -> Box<opcode::Disassembler> {
+	match arch_type {
+		&Some(ref arch) => {
+			match arch {
+				&opcode::Arch::Chip8 => Box::new(opcode::chip8::Chip8Disasm),
+				_ => panic!("Unknown machine"),
+			}
+		},
+		&None => {
+			panic!("Machine not set");
+		},
+	}
+}
+
+fn disassemble_segment(segment_meta: &exefmt::Segment, data: &Vec<u8>, parsed_options: &ObjdumpOptions) -> Result<(), std::io::Error> {
+	let mut residue: usize = segment_meta.file_size as usize;
+	let mut consumed: usize = 0;
+	let base = segment_meta.load_base + parsed_options.vma_offset;
+	let disassembler = disassembler_factory(&parsed_options.arch);
+
+	println!("");
+	println!("Disassembly of section {}", segment_meta.name);
+
+	println!("");
+	println!("{:08x} <{}>:", base, segment_meta.name);
+
+	while residue != 0 {
+		let (dis_text, num_bytes) = match disassembler.disassemble(base + (consumed as u64), &data.as_slice()[consumed .. data.len()]) {
+			Ok((dis_text, num_bytes)) => (dis_text, num_bytes),
+			Err(opcode::DisError::Unknown{ num_bytes }) => 
+					(format!(".word 0x{:02x}{:02x}", data[consumed], data[consumed+1]), num_bytes),
+			Err(opcode::DisError::MemOverflow) => {
+				println!("{:8x}:    {:02x}       .byte 0x{:02x}", base + (consumed as u64), data[consumed], data[consumed]);
+				consumed += 1;
+				residue -= 1;
+				continue;
+			},
+			Err(_) => {
+				return Err(Error::new(ErrorKind::Other, "Diassembly error"));
+			},
+		};
+		let byte_text = format!("{:02x} {:02x}", data[consumed], data[consumed+1]);
+		println!("{:8x}:    {}    {}", base + (consumed as u64), byte_text, dis_text);
+		consumed += num_bytes;
+		residue -= num_bytes;
+	}
+
+	Ok(())
+}
+
+fn do_objdump(file_name: &String, parsed_options: &ObjdumpOptions) -> Result<(), std::io::Error> {
+	let mut file = try!(std::fs::File::open(file_name));
+	let filter = segment_filter_factory(parsed_options);
+
+	let (segments, binfmt_name) = match parsed_options.bin_fmt {
+		BinaryFormat::NotSet => {
+			return Err(Error::new(ErrorKind::Other, "Unknown binary format"));
+		},
+		BinaryFormat::RawBinary => {
+			let ldr = try!(exefmt::binary::BinLoader::new(&mut file));
+			(try!(ldr.get_segments(&*filter, &mut file)), "binary".to_string())
+		},
+		BinaryFormat::Elf => {
+			return Err(Error::new(ErrorKind::Other, "Have yet to implement ELF loading"));
+		},
+	};
+
+	println!("");
+	println!("{}:     file format {}", file_name, binfmt_name);
+	println!("");
+
+	for (segment_meta, data) in segments {
+		match parsed_options.dis {
+			Disassemble::None => {},
+			Disassemble::Executable => { 
+				if segment_meta.executable {
+					try!(disassemble_segment(&segment_meta, &data, &parsed_options));
+				}
+			},
+			Disassemble::All => {
+				try!(disassemble_segment(&segment_meta, &data, &parsed_options));
+			},
+		}
+	}
+
+	Ok(())//parse_file(segments, parsed_options)
+}
+
 fn main() {
 	let args: Vec<String> = std::env::args().collect();
 	let program_name = args[0].clone();
@@ -165,8 +356,10 @@ fn main() {
 	let mut opts = getopts::Options::new();
 
 	match parse_opts(&args, &mut opts) {
-		Ok((_, _)) => {
-			println!("objdump");
+		Ok((parsed_options, file_names)) => {
+			for file_name in file_names {
+				do_objdump(&file_name, &parsed_options).unwrap();
+			}
 		},
 		Err(err) => {
 			on_err(err, &program_name);
